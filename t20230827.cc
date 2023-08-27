01f187765a51dc2e62a8bc40ce30c42e30575233
neverkah <240440826@qq.com>
2023-08-27 16:49:04 +0800

    add clang-tidy rule

--- t20230827.cc
+++ t20230827.cc
@@ -10,33 +10,32 @@ private:
 
     char count{0};
 public:
-    void setFamilyNo(const std::string &familyNo) {
-        Family::familyNo = familyNo;
+    std::string family_no_;
+
+    void setFamilyNo(const std::string &family_no) {
+        Family::family_no_ = family_no;
     }
 
-public:
-    std::string familyNo;
 
-    explicit Family(std::string familyNo) : familyNo(std::move(familyNo)) {}
+    explicit Family(std::string family_no) : family_no_(std::move(family_no)) {}
 };
 
 class Cat {
 private:
-    std::string name;
-    char xx{};
+    std::string name_;
 
 //    int age;
 public:
-    const Family &family;
+    const Family &family_;
 
     const std::string &getName() const {
-        return name;
+        return name_;
     }
 
-    explicit Cat(const Family &family) : family(family)  {
+    explicit Cat(const Family &family) : family_(family) {
     }
 
-    Cat(std::string name, char xx, const Family &family) : name(std::move(name)), xx(xx), family(family) {}
+    Cat(std::string name, const Family &family) : name_(std::move(name)), family_(family) {}
 };
 
 void testRef(Cat s);
@@ -44,7 +43,7 @@ void testRef(Cat s);
 int main() {
 
     Family fff{"abc"};
-    Cat cat{"a", 2, fff};
+    Cat cat{"a", fff};
     Cat *catp = &cat;
     std::cout << sizeof(Cat) << std::endl;
     std::cout << sizeof(Cat *) << std::endl;
@@ -56,7 +55,7 @@ int main() {
     Cat catf = Cat{f};
     std::string newNo = "newf";
     f = Family{newNo};
-    std::cout << catf.family.familyNo << std::endl;
+    std::cout << catf.family_.family_no_ << std::endl;
 
     std::cout << cat.getName() << std::endl;
 }
